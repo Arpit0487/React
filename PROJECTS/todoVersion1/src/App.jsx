1c04339774fd051a69ee0dff695bbeafdfa5bee0
Arpit0487 <arpitsh5568@gmail.com>
2026-02-17 18:35:48 +0530

    day 2

--- PROJECTS/todoVersion1/src/App.jsx
+++ PROJECTS/todoVersion1/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import AppName from "./components/AppName";
+import AddToDo from "./components/AddToDo";
+import ToDoItem1 from "./components/ToDoItem1";
+import ToDoItem2 from "./components/ToDoItem2";
+import "./App.css";
 
+function App() {
   return (
-    <h1>Arpit Sharma</h1>
-  )
+    <center class="todo-Container">
+      <AppName></AppName>
+      <AddToDo></AddToDo>
+      <div className="item-Container">
+        <ToDoItem1></ToDoItem1>
+        <ToDoItem2></ToDoItem2>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
---
+++ PROJECTS/todoVersion1/src/components/ToDoItem2.jsx
@@ -0,0 +1,19 @@
+function ToDoItem1() {
+    let name = 'Buy Oats';
+    let date = '5/10/23';
+    return(
+        <div>
+            <div class="row">
+            <div class="col-6">{name}</div>
+            <div class="col-4">{date}</div>
+            <div class="col-2">
+                <button type="button" class="btn btn-danger">
+                Delete
+                </button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default ToDoItem1;
